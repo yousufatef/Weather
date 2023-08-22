6abb0b329163f4928cb1c04dc719493bd079e251
Youssef <joeatef@gmail.com>
2023-08-22 08:46:51 +0300

    Uploading project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
+      <section className="bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] text-zinc-700">
+        <h1>Weather Forecast</h1>
+      </section>
+    </main>
+  )
+}
+
+export default App
