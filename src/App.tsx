e6b510a1fd117881ed39d6cafb9e37c90ac0583a
Youssef <joeatef@gmail.com>
2023-08-22 09:15:46 +0300

    message

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ const App = () => {
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
       <section className="bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] text-zinc-700">
         <h1>Weather Forecast</h1>
+        <p>Weather Forecastsdvsdvbsdvsdvsv</p>
       </section>
     </main>
   )
